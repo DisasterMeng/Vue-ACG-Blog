f3605dfef857193232e507348a9b4eb211e6deda
LingHanChuJian <2739069093@qq.com>
2019-09-01 23:13:48 +0800

    style: 修复blog详情页标题样式问题

--- src/components/blogDetails/blogHeader.vue
+++ src/components/blogDetails/blogHeader.vue
@@ -28,37 +28,36 @@ export default {
   height 475px
   background-size cover
   background-position center center
+  position relative
 
 .blog-details
   max-width 800px
-  margin 0 auto
-  padding-top 350px
+  position absolute
+  bottom 30px
+  left 360px
 
 .blog-title
   letter-spacing 3px
   font-weight bold
-  font-size 40px
+  font-size 32px
   color #FFFFFF
   text-shadow 2px 2px 10px #000000
   cursor default
 
 .details
   margin 10px 0 0 0
-
-.details a
-  color #FFFFFF
-
-.details span
-  color #FFFFFF
-  display inline-block
-  vertical-align middle
-  text-shadow 2px 2px 10px #000000
+  a
+    color #FFFFFF
+  span
+    color #FFFFFF
+    display inline-block
+    vertical-align middle
+    text-shadow 2px 2px 10px #000000
+  img
+    width 35px
+    height 35px
+    border-radius 100%
 
 .details span:nth-child(2)
   margin-left 5px
-
-.details img
-  width 35px
-  height 35px
-  border-radius 100%
 </style>
